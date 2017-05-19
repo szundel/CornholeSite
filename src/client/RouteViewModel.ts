e06aa0630d13f0eee53956d54bd3f8e457f5d458
Lucas <kadian1470@gmail.com>
2017-05-19 15:22:11 -0400

    Add www root

--- src/client/RouteViewModel.ts
+++ src/client/RouteViewModel.ts
@@ -5,11 +5,14 @@ module Route{
       var rootModel = new Root.RootViewModel();
       Sammy("#bindingContainer",function(){
           this.get("#/cornhole", context=>{
-              
-          })
+              //this.switchViews("cornhole.html", )
+          });
+
       });
     }
+    switchViews(htmlTemplate:string, viewModel:any){
 
+    }
     publishTitle(){
 
     };
